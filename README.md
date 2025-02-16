5fa0dc643d7dc3e2f0002a27770d4f2230331792
Sumit Saha <sumit@learnwithsumit.com>
2025-02-16 16:22:09 +0600

    Step 3 - Complex React State Management - with built-in `useState` hook

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MERN Stack Career Path - Batch 4 - Interactive Cares - Free Webinar on Redux
 
-Step 2 - Simple React State Management - with built-in `useState` hook
+Step 3 - Complex React State Management - with built-in `useState` hook
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+import initialCounters from "./data/initial-counters";
 
 function App() {
+    const [counters, setCounters] = useState(initialCounters);
+
+    const handleIncrement = (counterId) => {
+        const updatedCounters = counters.map((counter) => {
+            if (counter.id === counterId) {
+                return {
+                    ...counter,
+                    value: counter.value + 1,
+                };
+            }
+            return counter;
+        });
+
+        setCounters(updatedCounters);
+    };
+
+    const handleDecrement = (counterId) => {
+        const updatedCounters = counters.map((counter) => {
+            if (counter.id === counterId) {
+                return {
+                    ...counter,
+                    value: counter.value - 1,
+                };
+            }
+            return counter;
+        });
+
+        setCounters(updatedCounters);
+    };
+
+    const totalCount = counters.reduce(
+        (sum, counter) => sum + counter.value,
+        0
+    );
+
     return (
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -8,7 +46,16 @@ function App() {
             </h1>
 
             <div className="max-w-md mx-auto mt-10 space-y-5">
-                <Counter />
+                {counters.map((counter) => (
+                    <Counter
+                        key={counter.id}
+                        count={counter.value}
+                        onIncrement={() => handleIncrement(counter.id)}
+                        onDecrement={() => handleDecrement(counter.id)}
+                    />
+                ))}
+
+                <Stats count={totalCount} />
             </div>
         </div>
     );
